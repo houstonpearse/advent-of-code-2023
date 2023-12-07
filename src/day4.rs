0d821e9cc58439f1f34649a20048eeaef0d203a3
houy <67390187+houstonpearse@users.noreply.github.com>
2023-12-07 14:18:19 +1100

    day4 solution2

--- src/day4.rs
+++ src/day4.rs
@@ -34,11 +34,25 @@ pub fn solution2() -> i32 {
         println!("{}",lines_result.err().unwrap());
         panic!("Error Reading File");
     };
-    let mut sum: i32 = 0;
+    let mut cards: Vec<Card> = Vec::new();
     for line in lines {
         if let Ok(card_string) = line {
+            cards.push(Card::from(card_string));
         }
     };
+    for index in 0..cards.len() {
+        let matches = cards[index].get_matches();
+        for i in index+1..index+1+matches as usize {
+            if i < cards.len() {
+                cards[i].copies += cards[index].copies;
+            }
+        }
+    }
+    let mut sum: i32 = 0;
+    for card in cards {
+        sum+=card.copies;
+    }
+
     return sum
 }
 
@@ -50,20 +64,14 @@ where P: AsRef<Path>, {
 
 #[derive(Debug)]
 struct Card {
-    id: i32,
     win_nums: Vec<i32>,
-    card_nums: Vec<i32>
+    card_nums: Vec<i32>,
+    copies: i32,
 }
 
 impl Card {
     fn from(card: String) -> Card {
         let card_split: Vec<&str> = card.split(":").collect();
-        let card_num:i32 = card_split[0]
-            .split_whitespace()
-            .nth(1)
-            .unwrap()
-            .parse::<i32>()
-            .unwrap();
         let numbers: Vec<&str> = card_split[1].split("|").collect();
         let win_nums: Vec<i32> = numbers[0]
             .trim()
@@ -75,7 +83,17 @@ impl Card {
             .split_whitespace()
             .map(|num| num.parse::<i32>().unwrap())
             .collect();
-        return Card {id: card_num, win_nums, card_nums};
+        return Card {win_nums, card_nums, copies: 1};
+    }
+
+    fn get_matches(&self) -> i32 {
+        let mut matches: i32 = 0;
+        for num in self.card_nums.iter() {
+            if self.win_nums.contains(num) {
+                matches+=1;
+            }
+        }
+        return matches;
     }
 
 }
