261766a0285b3c773b63caeb2a9ce618924da501
houy <67390187+houstonpearse@users.noreply.github.com>
2024-01-11 14:30:33 +1100

    day6 solution2

--- src/day6.rs
+++ src/day6.rs
@@ -16,6 +16,31 @@ pub fn solution1() -> i32 {
     return result
 }
 
-pub fn solution2() -> i32 {
-    return 0
+pub fn solution2() -> i64 {
+    let input: Vec<String> = read_to_string("./inputs/input6.txt")
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect();
+    let time: i64 = input[0]
+        .split_whitespace()
+        .filter(|x| x.parse::<i32>().is_ok())
+        .fold(String::new(),|acc, e| acc + e)
+        .parse::<i64>()
+        .unwrap();
+    let record: i64 = input[1]
+        .split_whitespace()
+        .filter(|x| x.parse::<i32>().is_ok())
+        .fold(String::new(),|acc, e| acc + e)
+        .parse::<i64>()
+        .unwrap();
+
+    // sec * (time - sec) > record
+    // - sec^2 + time*sec - record > 0
+    // sec^2 - time*sec + record < 0
+    // time/2 - root(time^2-4*record)/2 < sec <  = time/2 + root((time^2-4*record)/2
+    let discriminant = f64::sqrt((((time.pow(2))-4*record)/4) as f64);
+    let lower = (time/2) as f64 - discriminant;
+    let upper = (time/2) as f64 + discriminant;
+    return upper as i64 + 1 - (lower as i64 + 1);
 }
